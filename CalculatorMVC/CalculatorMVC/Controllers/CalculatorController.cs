0d56b451885cad38706068f126e8bff04b511b44
Isupov Nikolay <kedichboss@gmail.com>
2021-08-31 21:05:05 +0300

    Project files added

---
+++ CalculatorMVC/CalculatorMVC/Controllers/CalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data;
+
+namespace CalculatorMVC.Controllers
+{
+    [Controller]
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("[controller]/Evaluate/{expressionToCalculate}")]
+        public ActionResult<double> Calculate([FromRoute] string expressionToCalculate)
+        {
+            string str = expressionToCalculate.Replace('÷', '/');
+            var result = Convert.ToDouble(new DataTable().Compute(str, null));
+            return Ok(result);
+        }
+    }
+}
---
+++ CalculatorMVC/CalculatorMVC/wwwroot/js/calculator.js
@@ -0,0 +1,40 @@
+﻿const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', calculate);
+});
+
+function calculate(event) {
+
+    const clickedButtonValue = event.target.value;
+    if (clickedButtonValue === '=') {
+        if (display.value.includes('÷0')) {
+            alert('You can\'t divide by zero')
+            display.value = '';
+        }
+        if (display.value !== '' && display.value.slice(-1) !== '+' && display.value.slice(-1) !== '-' && display.value.slice(-1) !== '*' && display.value.slice(-1) !== '÷') {
+            display.value = Evaluate(display.value);
+        }
+    } else if (clickedButtonValue === 'C') {
+        display.value = '';
+    } else {
+        if (!((clickedButtonValue === '+' || clickedButtonValue === '-' || clickedButtonValue === '*' || clickedButtonValue === '÷') &&
+            (display.value === '' || display.value.slice(-1) === '+' || display.value.slice(-1) === '-' || display.value.slice(-1) === '*' || display.value.slice(-1) === '÷'))) {
+            display.value += clickedButtonValue;
+        }
+    }
+}
+//TODO jQuery + ajax
+function Evaluate(expression) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', 'Calculator/Evaluate/' + expression, false);
+    xmlHttp.send();
+    xmlHttp.onload = function () {
+        if (xmlHttp.status != 200) {
+            alert(`Error ${xmlHttp.status}: ${xmlHttp.statusText}`);
+        }
+    };
+    if (xmlHttp.readyState === 4)
+        return xmlHttp.response;
+}
